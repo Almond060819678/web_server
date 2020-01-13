ff40868e018c4912b594e33cba156c55f60beed0
Chudakov <mikhail.chudakov@deliveryweb.ru>
2020-01-13 14:59:08 +0300

    fix error

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -147,6 +147,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 
 
 if __name__ == '__main__':
-    server_address = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 8000))
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
